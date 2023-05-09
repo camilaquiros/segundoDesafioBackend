0b4e6fceed2c8c14c2956e1e6c604010cfd1dafb
CamilaQ <camila.quiros.07@gmail.com>
2023-05-09 04:55:51 -0300

    entrega

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const fs = require('fs')
 
 class ProductManager {
-    // products = cargaDelArchivo();6
+    products = [];
     product = Product;
     constructor(path){
         this.path = path;
-    }
-
-    guardarCambios() {
-        fs.writeFileSync(path, JSON.stringify(products))
     }
 
     cargaDelArchivo(){
@@ -24,36 +20,54 @@ class ProductManager {
     }
 
     addProduct(product) {
-        const database = this.cargaDelArchivo();4
-        if (this.getProducts().find((p) => p.code == product.code)){ 
+        const database = this.cargaDelArchivo();
+        if (database.find((p) => p.code == product.code)){ 
             console.log(`Producto ${product.title} ya existente`);
         } else{
-            product.id = this.getProducts().length + 1;
-            this.products.push(product);
-            this.guardarCambios();
+            product.id = database.length + 1;
+            database.push(product);
+            fs.writeFileSync(this.path, JSON.stringify(database))
         }
     }
 
-    // getProductsById(product) {
-    //     this.cargaDelArchivo();
-    //     if(this.getProducts().find((search) => search.id == product)){
-    //         return console.log(this.getProducts().find((search) => search.id == product))
-    //     } else{
-    //         return console.log('Producto no encontrado')
-    //     }
-    // }
-
-    updateProduct(){
-
+    getProductsById(productId) {
+        const database = this.cargaDelArchivo();
+        const product = database.find((search) => search.id == productId)
+        if(product){
+            return console.log(product)
+        } else{
+            return console.log('Producto no encontrado')
+        }
     }
 
-    deleteProduct(){
+    updateProduct(productUpdate, {...productNew}){
+        const database = this.cargaDelArchivo();
+        this.deleteProduct(productUpdate);
+        const product = database.find((search) => search.id == productUpdate)
+        if(product){
+            this.deleteProduct(productUpdate);
+            const databaseNueva = this.cargaDelArchivo();
+            let productsModified = [{ ...productNew, id: productUpdate}, ...databaseNueva]
+            fs.writeFileSync(this.path, JSON.stringify(productsModified))
+        } else{
+            return console.log('Producto no encontrado')
+        }
+    }
 
+    deleteProduct(product){
+        const database = this.cargaDelArchivo();
+        if(database.find((search) => search.id == product)){
+            const databaseNueva = database.filter((search) => search.id !== product)
+            fs.writeFileSync(this.path, JSON.stringify(databaseNueva))
+        } else{
+            return console.log('Producto no encontrado')
+        }
     }
 }
 
 class Product extends ProductManager {
     contructor(title, description, price, thumbnail, code, stock) {
+        this.id = id;
         this.title = title;
         this.description = description;
         this.price = price;
@@ -65,15 +79,49 @@ class Product extends ProductManager {
 
 const nuevoProducto = new ProductManager('./database.json');
 
+const product1 = {
+    title: 'celular',
+    description: 'Lorem Ipsum',
+    price: 1000,
+    thumbnail: 'thumbnail',
+    code: 'abc123',
+    stock: 10
+}
+
+const product2 = {
+    title: 'computadora',
+    description: 'Lorem Ipsum',
+    price: 5000,
+    thumbnail: 'thumbnail',
+    code: 'abc124',
+    stock: 10
+}
+
+const product3 = {
+    title: 'tablet',
+    description: 'Lorem Ipsum',
+    price: 3000,
+    thumbnail: 'thumbnail',
+    code: 'abc125',
+    stock: 10
+}
+
 console.log(nuevoProducto.getProducts())
 
-// nuevoProducto.addProduct(product1);
-// nuevoProducto.addProduct(product2);
-// nuevoProducto.addProduct(product3);
+nuevoProducto.addProduct(product1);
+nuevoProducto.addProduct(product2);
+nuevoProducto.addProduct(product3);
 
-// console.log(nuevoProducto.getProducts())
+console.log(nuevoProducto.getProducts())
 
-// nuevoProducto.addProduct(product3SameCode);
+// console.log(nuevoProducto.deleteProduct(2))
+// console.log(nuevoProducto.deleteProduct(4))
 
-// console.log(nuevoProducto.getProductsById(2))
-// console.log(nuevoProducto.getProductsById(4))
+// console.log(nuevoProducto.updateProduct(1,  
+// {
+// title: 'celular',    
+// description: 'Lorem Ipsum',
+// price: 7000,
+// thumbnail: 'thumbnail',
+// code: 'abc124',
+// stock: 10}))
